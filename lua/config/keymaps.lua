b0c75b6bf830b355212f6eb3b3217aee9a722cb4
Pavel Dashkovskii <pavel.dashkovskii@arenza.ru>
2024-05-27 21:53:45 +0300

    Clean keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-local map = LazyVim.safe_keymap_set
-map("n", "<leader>.", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-map("n", "<C-,>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })

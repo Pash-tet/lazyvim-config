4aad60b105456fc3fde5b7582b89b584e5f2c309
Pavel Dashkovskii <exgroover@gmail.com>
2024-05-29 16:07:03 +0300

    Add slim and colorscheme

---
+++ lua/plugins/coloscheme.lua
@@ -0,0 +1,24 @@
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+
+return {
+  {
+    "catppuccin/nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
